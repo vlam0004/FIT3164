5447d3a18af04be7728c5894cba340a88490e2ca
Manan-python <mana0007@student.monash.edu>
2021-10-21 23:05:35 +0530

    tried testing

--- apps/test_sample.py
+++ apps/test_sample.py
@@ -1,10 +1,16 @@
 import pandas as pd
-from home import update_cases
+from home import update_cases, update_deaths
 
 data1 = 'https://raw.githubusercontent.com/owid/covid-19-data/master/public/data/owid-covid-data.csv'
 df1 = pd.read_csv(data1)
 total_cases1 = df1.loc[df1.location == 'World', ['total_cases']].max()
+total_deaths2 = df1.loc[df1.location == 'World', ['total_deaths']].max()
 
 
 def test_update_cases():
-    assert update_cases(1) == total_cases1
+    result = update_cases.__wrapped__(1)
+    assert (result == total_cases1).all()
+
+def test_update_deaths():
+    result = update_deaths.__wrapped__(1)
+    assert (result == total_deaths2).all()
